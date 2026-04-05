5b9398f659a18f158fc316d73cbd2eb9f727c447
Rudra Pratap <rudrasikarwar.dev@gmail.com>
2026-04-05 12:36:30 +0530

    Day 5: Implemented ViewModel with LiveData

---
+++ app/src/main/java/com/example/notesapp/NoteViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.notesapp
+
+import android.app.Application
+import androidx.lifecycle.*
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NoteRepository
+
+    val allNotes = MutableLiveData<List<Note>>()
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).noteDao()
+        repository = NoteRepository(dao)
+        loadNotes()
+    }
+
+    fun loadNotes() = viewModelScope.launch {
+        allNotes.value = repository.getAllNotes()
+    }
+
+    fun insert(note: Note) = viewModelScope.launch {
+        repository.insert(note)
+        loadNotes()
+    }
+
+    fun update(note: Note) = viewModelScope.launch {
+        repository.update(note)
+        loadNotes()
+    }
+
+    fun delete(note: Note) = viewModelScope.launch {
+        repository.delete(note)
+        loadNotes()
+    }
+}
