271cab698b7a445a63bd7db66797ad3481997433
Rudra Pratap <rudrasikarwar.dev@gmail.com>
2026-04-21 17:24:51 +0530

    Fixed splash screen UI by removing action bar and applying custom theme

--- app/src/main/java/com/example/notesapp/SplashActivity.kt
+++ app/src/main/java/com/example/notesapp/SplashActivity.kt
@@ -8,13 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.os.Handler
+import android.view.View
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val rootView = findViewById<View>(android.R.id.content)
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
